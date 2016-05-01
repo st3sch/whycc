0f806f213173c159f9298ae49ef271d684a93b52
Stefan Schwierz <st3sch@gmail.com>
2016-05-01 23:22:18 +0200

    Ok first simple parsing is working.

--- whycc.go
+++ whycc.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"encoding/csv"
+	"os"
+	"bufio"
+	"io"
+	"log"
+)
 
 func main() {
-	fmt.Println("Hello whycc")
+	f,_ := os.Open("./testdata/Umsatzanzeige_1234567890_20160410.csv")
+	r := csv.NewReader(bufio.NewReader(f))
+	r.Comma =';'
+	r.FieldsPerRecord = -1
+	for {
+		record, err := r.Read()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		if len(record) != 9 || record[0] == "Buchung" {
+			continue
+		}
+
+		fmt.Println(record)
+	}
 }
+
+
+
