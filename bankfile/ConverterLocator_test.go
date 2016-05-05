f4bf09a6d0f4af552be061328b33e7090468be17
Stefan Schwierz <st3sch@gmail.com>
2016-05-05 14:10:33 +0200

    Factory test finished.

--- bankfile/ConverterLocator_test.go
+++ bankfile/ConverterLocator_test.go
@@ -1,12 +1,34 @@
 package bankfile
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestFactory_FindBy(t *testing.T) {
+	type testpair struct {
+		in       string
+		expected string
+	}
+
+	tests := []testpair{
+		{"ingdiba", "IngDiBa"},
+	}
 
 	f := factory{}
-	_, err := f.FindBy("not existent")
+	for _, pair := range tests {
+		cl, err := f.FindBy(pair.in)
+		if err != nil {
+			t.Errorf("Unexpected Error: '%v'", err)
+			continue
+		}
+		if pair.expected != reflect.TypeOf(cl).Name() {
+
+		}
+	}
+
+	_, err := f.FindBy("non existent")
 	if err == nil {
-		t.Error("Expected error not thrown")
+		t.Error("Expected invlaid type error not raised")
 	}
 }
