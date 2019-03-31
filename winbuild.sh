52ea58b0183fc40232c58d240517250d1f6fa5a6
Stefan Schwierz <st3sch@gmail.com>
2019-03-31 23:35:41 +0200

    Add linker flags to windows build script

--- winbuild.sh
+++ winbuild.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-GOOS=windows GOARCH=amd64 go build -o whycc.exe whycc.go
+VERSION=`git describe --abbrev=0 --tags`
+COMMIT=`git log --pretty=format:'%h' -n 1`
+DATE=`date +"%Y%m%d%H%M%S"`
+GOOS=windows GOARCH=amd64 go build -ldflags "-X main.version=$VERSION -X main.commit=$COMMIT -X main.date=$DATE" -o whycc.exe whycc.go
