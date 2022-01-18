3af0b824ee9b01108cd0fd099510ed642f07ec42
Stefan Schwierz <st3sch@gmail.com>
2022-01-18 16:01:27 +0000

    Added failing test for 17 fields

--- bankfile/converter/augusta_test.go
+++ bankfile/converter/augusta_test.go
@@ -20,6 +20,7 @@ func TestAugusta_IsTransaction(t *testing.T)  {
 	
 	tests := []testpair{
 		{false, []string{}},
+		{true, []string{"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17"}},
 	}
 
 	k := NewAugusta()
